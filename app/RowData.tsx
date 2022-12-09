8786f369bdaf7b042fe6c64eddcdfb22eafb7566
ImMyst <titouan.galvani@yeap.ai>
2022-12-09 20:25:15 +0100

    fix: adjustments

--- app/RowData.tsx
+++ app/RowData.tsx
@@ -10,7 +10,7 @@ export default function RowData({ match }: TProps) {
   return (
     <div className="flex relative border-b flex-wrap border-gray-200 justify-between px-8 py-2">
       {match.status === "in_progress" && (
-        <span className="absolute left-3 flex h-3 w-3">
+        <span className="absolute left-5 top-3 flex h-3 w-3">
           <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-red-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
         </span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,21 @@
 import emojiFlags from "emoji-flags";
-import Image from "next/image";
 import { getUniqueListBy } from "../utils/getUniqueListBy";
 import RowData from "./RowData";
 import Tag from "./Tag";
 
 type TStatus = "in_progress" | "futur_scheduled" | "completed";
+type TTeam = {
+  code: string;
+  name: string;
+  score: number;
+  winner: boolean;
+};
 
 export type TResults = {
   id: number;
   status: TStatus;
-  firstTeam: {
-    code: string;
-    name: string;
-    flag: string;
-    score: number;
-    winner: boolean;
-  };
-  secondTeam: {
-    code: string;
-    name: string;
-    flag: string;
-    score: number;
-    winner: boolean;
-  };
+  firstTeam: TTeam;
+  secondTeam: TTeam;
 };
 
 export default async function Page() {
@@ -45,14 +38,12 @@ export default async function Page() {
         firstTeam: {
           code: match.home_team.country,
           name: match.home_team.name,
-          flag: "",
           score: match.home_team.goals,
           winner: match.home_team.country === match.winner_code,
         },
         secondTeam: {
           code: match.away_team.country,
           name: match.away_team.name,
-          flag: "",
           score: match.away_team.goals,
           winner: match.away_team.country === match.winner_code,
         },
@@ -78,6 +69,7 @@ export default async function Page() {
       <h1 className="text-3xl font-semibold">⚽️ Kup</h1>
       <div className="mb-2 overflow-hidden">
         <h2 className="text-xl mt-4 mb-2 font-semibold">Planning</h2>
+        <p className="mt-2 mb-2 font-semibold text-gray-700">Teams</p>
         <div className="flex space-x-2 overflow-auto touch-auto pb-4 flex-nowrap">
           {getTeams().map((team) => (
             <Tag key={team.code}>
