2d043efbc59ee62880cd4de2044e4243e4bbbe76
ImMyst <titouan.galvani@yeap.ai>
2022-12-09 13:40:00 +0100

    chore(boilerplate): adding content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Page() {
   return (
     <div className="flex justify-center h-screen w-full items-center">
-      <h1 className="text-3xl">
-        Salut
-      </h1>
+      <h1 className="text-3xl">Kup</h1>
     </div>
-  )
+  );
 }
