58f49be94cabf23e40916c563ea4599f3b96c285
ImMyst <titouan.galvani@yeap.ai>
2022-12-14 20:11:24 +0100

    fix(style): fixing absolute position in progress match

--- app/components/RowData.tsx
+++ app/components/RowData.tsx
@@ -10,7 +10,7 @@ export default function RowData({ match }: TProps) {
   return (
     <div className="flex relative items-center border-b flex-nowrap border-gray-200 whitespace-nowrap justify-between px-2 py-2">
       {match.status === "in_progress" && (
-        <span className="absolute left-5 top-3 flex h-3 w-3">
+        <span className="absolute left-3 top-3 flex h-3 w-3">
           <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-red-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
         </span>
